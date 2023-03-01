1f3ff31856fa46b0f96dfaa707c9b20e2e0b4373
Sercos10 <sergiocosano98@gmail.com>
2023-03-01 08:46:54 +0100

    control de errores hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Estado } from './model/call/Enum_call';
 import {  Observable, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+declare var bootstrap:any;
 
 @Component({
   selector: 'app-root',
@@ -29,50 +30,80 @@ export class AppComponent implements OnInit, OnDestroy {
   
   ngOnInit(): void {
     //muestra loading
-
-    this.peerId = this.callService.initPeer(); 
-    //Servicio de control de UI
-    this.subscriptionState=this.callService.stateCalling$.subscribe(state=>{
-      switch(state){
-        case 0:
-          //document.getElementById("colgar")?.click();
-          this.state=state;
-          console.log("ESTADO INICIAL");
-          //poner el botón rojo gordo
-          break;
-        case 1:
-          this.state=state;
-          console.log("ESTADO LLAMANDO")
-          //poner la animación verde y cancelar
-          break;
-        case 2:
-          this.state=state;
-          console.log("EN LLAMADA")
-          //poner la llmada y el colgar
-          break;
-        default: console.log("ESTADO DESCONOCIDO"); break;
-      }
-      this.cdr.detectChanges();
-    })
-
-    //Recoge la webcam y el microfono del navegador de manera local
-    this.callService.localStream$
-      .pipe(filter(res => !!res))
-      .subscribe(stream => this.localVideo.nativeElement.srcObject = stream)
+    try {
+      this.peerId = this.callService.initPeer(); 
+    } catch (error) {
+      const peerToast = document.getElementById('peerToast');
+        const toast = new bootstrap.Toast(peerToast);
+        toast.show()
+        console.log(error)
+    }
     
-    //Recoge la webcam y el microfono del equipo remoto
-    this.callService.remoteStream$
-      .pipe(filter(res => !!res))
-      .subscribe(stream => this.remoteVideo.nativeElement.srcObject = stream)
+    //Servicio de control de UI
+    try{
 
+      this.subscriptionState=this.callService.stateCalling$.subscribe(state=>{
+        switch(state){
+          case 0:
+            //document.getElementById("colgar")?.click();
+            this.state=state;
+            console.log("ESTADO INICIAL");
+            //poner el botón rojo gordo
+            break;
+          case 1:
+            this.state=state;
+            console.log("ESTADO LLAMANDO")
+            //poner la animación verde y cancelar
+            break;
+          case 2:
+            this.state=state;
+            console.log("EN LLAMADA")
+            //poner la llmada y el colgar
+            break;
+          default: console.log("ESTADO DESCONOCIDO"); break;
+        }
+        this.cdr.detectChanges();
+      })
+
+    } catch (error) {
+      const windowToast = document.getElementById('windowToast');
+        const toast = new bootstrap.Toast(windowToast);
+        toast.show()
+        console.log(error)
+    }
+
+    try{
+      //Recoge la webcam y el microfono del navegador de manera local
+      this.callService.localStream$
+        .pipe(filter(res => !!res))
+        .subscribe(stream => this.localVideo.nativeElement.srcObject = stream)
+      
+      //Recoge la webcam y el microfono del equipo remoto
+      this.callService.remoteStream$
+        .pipe(filter(res => !!res))
+        .subscribe(stream => this.remoteVideo.nativeElement.srcObject = stream)
+
+    } catch (error) {
+      const streamToast = document.getElementById('streamToast');
+        const toast = new bootstrap.Toast(streamToast);
+        toast.show()
+        console.log(error)
+    }
       //oculta animación
   }
   
   ngOnDestroy(): void {
     //desconecta la llamada
-    this.callService.destroyPeer();
-    if(this.subscriptionState){
-      this.subscriptionState.unsubscribe();
+    try{
+      this.callService.destroyPeer();
+      if(this.subscriptionState){
+        this.subscriptionState.unsubscribe();
+      }
+    } catch (error) {
+      const endCallToast = document.getElementById('endCallToast');
+        const toast = new bootstrap.Toast(endCallToast);
+        toast.show()
+        console.log(error)
     }
 
   }
@@ -95,18 +126,41 @@ export class AppComponent implements OnInit, OnDestroy {
     try{
       await this.callService.stablishCall(call);
     }catch(err){
+      const postToast = document.getElementById('postToast');
+      const toast = new bootstrap.Toast(postToast);
+      toast.show()
       console.error(err);
     }
-    this.callService.enableCallAnswer();
+    try{
+      this.callService.enableCallAnswer();
+    } catch (error) {
+      const callToast = document.getElementById('callToast');
+      const toast = new bootstrap.Toast(callToast);
+      toast.show()
+      console.log(error)
+    }
   }
 
   public async cancelCall(){
-    await this.callService.cancelCall();
+    try{
+      await this.callService.cancelCall();
+    } catch (error) {
+      const endCallToast = document.getElementById('endCallToast');
+        const toast = new bootstrap.Toast(endCallToast);
+        toast.show()
+        console.log(error)
+    }
   }
 
   //metodo para terminar la llamada
   public endCall() {
-    this.callService.closeMediaCall();
-    
+    try{
+      this.callService.closeMediaCall();
+    } catch (error) {
+      const endCallToast = document.getElementById('endCallToast');
+        const toast = new bootstrap.Toast(endCallToast);
+        toast.show()
+        console.log(error)
+    }
   }
 }
